9bff1f61c685ef035ba4ab3c9d456af2652319d7
long-Bill <telleph0ne34@gmail.com>
2024-09-11 18:39:20 -0700

    sql changes

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import streamlit as st
 from openai import OpenAI
 import random
 import time
+import mysql.connector as mysql
+
+def databaseConnection(dbhost, dbuser, dbpassword):
+    conn = mysql.connect(
+        host = dbhost,
+        user = dbuser,
+        passwd = dbpassword
+    )
+    return conn
+def sqlExecution(query,cursor):
+    try:
+        cursor.execute(query)
+        for x in cursor.fetchall():
+            print(x)
+    except:
+        print("There was an issue with either the sql query or command was fetchable")
+    
 
 #Change page title (shown on browser tab)
 st.set_page_config(page_title="SWIFT's Chatbot")
@@ -48,27 +65,51 @@ if not st.session_state.messages:
     with st.chat_message("assistant"):
         greetings = st.write_stream(response_generator())
     st.session_state.messages.append({"role": "assistant", "content": greetings})
+
 if prompt := st.chat_input("What is up?"):
     
     # Display user message in chat message container
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-    
-    
+   
+    #Matches for the "sql" command to enter the sql mode
+    numberOfSQL = sum([d['content'] == 'sql' for d in st.session_state.messages])
+    if (numberOfSQL % 2 > 0): 
+        db = databaseConnection("192.168.1.120","ccdc","ccdc")
+        cursor = db.cursor()
+        if(prompt == 'sql'):
+            sqlMessage = "Entering SQL mode"          
+            st.chat_message("assistant").markdown(sqlMessage)
+            st.session_state.messages.append({"role": "assistant", "content": sqlMessage})
+        
+        else:
+            try:
+                cursor.execute(prompt)
+            except:
+                print("SQL query is unrecognizeable")
+            else:
+                db.close()
+
+        
+    else:
     # Display assistant response in chat message container
-    if len(st.session_state.messages) >= 2:
-        with st.chat_message("assistant"):
-            stream = client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            )
-        # Add assistant response to chat history
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        if ((len(st.session_state.messages) >= 2) & (prompt != 'sql')) :
             
+            with st.chat_message("assistant"):
+                stream = client.chat.completions.create(
+                    model=st.session_state["openai_model"],
+                    messages=[
+                        {"role": m["role"], "content": m["content"]}
+                        for m in st.session_state.messages
+                    ],
+                    stream=True,
+                )
+            # Add assistant response to chat history
+                response = st.write_stream(stream)
+            st.session_state.messages.append({"role": "assistant", "content": response})
+        else:     
+            exitSQL = "Connection to database has closed"
+            st.chat_message("assistant").markdown(exitSQL)
+            st.session_state.messages.append({"role": "assistant", "content": exitSQL})
 

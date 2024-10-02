a044e21c847a84ac36fcda4cc993d825ed47ebde
long-Bill <telleph0ne34@gmail.com>
2024-10-01 23:44:43 -0700

    Return error message

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from openai import OpenAI
 import random
 import os
+import sys
 import time
 import mysql.connector as mysql
 import traceback
@@ -17,8 +18,6 @@ def databaseConnection(dbhost, dbuser, dbpassword):
     return conn
 def sqlExecution(query,cursor):
 
-    #if ("databases" in query.casefold()) | ('database' in query.casefold()):
-    #Prvovide extra information like selecting from tableXX....
     listOfWords = query.split()
     if ("show" in query.casefold()):
         messages = f"Displaying {listOfWords[1].replace(';','')}....  \n"
@@ -30,8 +29,15 @@ def sqlExecution(query,cursor):
         for x in cursor.fetchall():
             messages += f'{x}  \n'
         return messages
-    except Exception:
-        return traceback.format_exc()
+        
+    except mysql.errors.ProgrammingError: 
+        return f'Error in "{query}" command. Please exit sql mode by typing "/sql" and ask me more questions.'
+    # except Exception as e:
+    #     exc_type, exc_value, exc_tb = sys.exc_info() 
+    #     tb = traceback.TracebackException(exc_type, exc_value,exc_tb) 
+    #     #
+    #     return ''.join(tb.format_exception_only())
+        
     # else:
     #     try:
     #         cursor.execute(query)
@@ -86,7 +92,7 @@ if not st.session_state.messages:
         greetings = st.write_stream(response_generator())
     st.session_state.messages.append({"role": "assistant", "content": greetings})
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Type here..."):
     
     # Display user message in chat message container
     st.chat_message("user").markdown(prompt)
@@ -96,16 +102,16 @@ if prompt := st.chat_input("What is up?"):
     #Matches for the "sql" command to enter the sql mode
     numberOfSQL = sum([d['content'] == sqlCommand for d in st.session_state.messages])
     if (numberOfSQL % 2 > 0): 
-        db = databaseConnection("192.168.1.120","ccdc","ccdc")
+        db = databaseConnection(st.secrets["host"],st.secrets["user"],st.secrets["password"])
         cursor = db.cursor()
         if(prompt == sqlCommand):
-            sqlMessage = 'Entering SQL mode...  \nAvailable commands:  \nSELECT and SHOW'          
+            sqlMessage = 'Entering SQL mode...  \nPlease type "/sql" again to exit sql mode  \nAvailable commands:  \nSELECT and SHOW'          
             st.chat_message("assistant").markdown(sqlMessage)
             st.session_state.messages.append({"role": "assistant", "content": sqlMessage})
         
         else:
             try:
-                sqlResponse = sqlExecution(prompt, cursor)
+                sqlResponse = sqlExecution(prompt.casefold(), cursor)
                 st.chat_message("assistant").markdown(sqlResponse)
                 st.session_state.messages.append({"role": "assistant", "content": sqlResponse})
             except:
